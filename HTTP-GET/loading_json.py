d81ddc2549bdb7cb8bc1d3f4b43daa29ada56987
Elvin Ramirez <real8686@gmail.com>
2018-08-23 23:12:49 -0400

    using json dumps

--- HTTP-GET/loading_json.py
+++ HTTP-GET/loading_json.py
@@ -13,3 +13,17 @@ print('-------------------------------------')
 data = json.loads(text_json)
 
 print(type(data),data)
+
+# Pull instructor data
+
+instructor = data.get('instructor','SUBSTITUTE')
+
+print("Your Instructor is {} ....".format(instructor))
+
+# testing the json change data
+
+data['instructor'] =  'Jeff'
+
+new_json = json.dumps(data)
+
+print(type(new_json),new_json)
