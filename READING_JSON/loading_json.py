d6e1e7fe0a0b31803a764e92fbb7e2dc49523ed3
linuxbytes <real8686@gmail.com>
2019-03-21 21:22:40 -0400

    add get in code

--- READING_JSON/loading_json.py
+++ READING_JSON/loading_json.py
@@ -1,17 +1,33 @@
 import json
+import datetime
 
-text_json = '''{
+
+text_json = """{
     "demo": "Processing JSON in Python",
     "instructor": "Michael",
     "duration": 5.0
     }
-'''
+"""
 
 print(type(text_json), text_json)
 
+print()
+
 data = json.loads(text_json)
 
-print(data)
+print(type(data), data)
 
+print()
+
+instructor = data.get("instructor", "Nothing Found")
+
+print()
+
+print("instructor is: {}".format(instructor))
 
 print(data["demo"])
+
+print()
+data["time"] = str(datetime.datetime.now())
+
+print(data)
